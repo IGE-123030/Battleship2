9bf0b3af98fde63aa4dee65cc7776be700e1b5a9
mirfa <mirfa1@iscte-iul.pt>
2026-04-23 15:37:47 +0100

    battleship.Position::adjacentPositions	Long Method (Inicialização manual de matriz de direções)	Extract Method

--- src/main/java/battleship/Position.java
+++ src/main/java/battleship/Position.java
@@ -1,5 +1,7 @@
 package battleship;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -139,20 +141,8 @@ public class Position implements IPosition {
 		int row = this.getRow();
 		int col = this.getColumn();
 
-		// Define possible directions (up, right, down, left)
-		int[][] directions = {
-				{-1, 0},  // north
-				{0, 1},   // east
-				{1, 0},   // south
-				{0, -1},   // west
-				{1, 1},   // northeast
-				{1, -1},  // northwest
-				{-1, 1},  // southeast
-				{-1, -1} // southwest
-		};
-
-		// Check each possible direction
-		for (int[] dir : directions) {
+        // Check each possible direction
+		for (int[] dir : getDirections()) {
 			Position newPosition = new Position(row + dir[0], col + dir[1]);
 			// Only add the position if it's inside the board boundaries
 			if (newPosition.isInside()) {
@@ -163,7 +153,21 @@ public class Position implements IPosition {
 		return adjacents;
 	}
 
-	/**
+    private int[] @NotNull [] getDirections() {
+        int[][] directions = {
+                {-1, 0},  // north
+                {0, 1},   // east
+                {1, 0},   // south
+                {0, -1},   // west
+                {1, 1},   // northeast
+                {1, -1},  // northwest
+                {-1, 1},  // southeast
+                {-1, -1} // southwest
+        };
+        return directions;
+    }
+
+    /**
 	 * Checks if this position is occupied by a ship.
 	 *
 	 * @return true if the position is occupied, false otherwise
